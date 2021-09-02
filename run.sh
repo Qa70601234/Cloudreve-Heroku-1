775e0e7ae931e64dfffffd17a7bb36f8a4fc4b23
Qa70601234 <74185784+Qa70601234@users.noreply.github.com>
2021-09-02 13:29:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 cat <<-EOF > /root/cloudreve/conf.ini
 [System]
 ; 运行模式
@@ -13,6 +12,4 @@ SessionSecret = oXIYVNP23Z9cXRCqn8C8z8etztNo5E33Qd6n6q3TFe3Z7aPPSer4nOEmADwljdIh
 ; Hash 加盐, 一般在首次启动时自动生成
 HashIDSalt = 2je20slhtLqQXalz7a0mCA9hU0H88B0D7vy3jiW4Zw97z35VBeglaFuDBEwmNma9
 EOF
-
-
 /root/cloudreve/cloudreve -c /root/cloudreve/conf.ini

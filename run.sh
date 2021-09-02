3d0412c42780844347e2d175714ed8d07f0f38b3
Qa70601234 <74185784+Qa70601234@users.noreply.github.com>
2021-09-02 22:39:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,5 +11,18 @@ Debug = false
 SessionSecret = oXIYVNP23Z9cXRCqn8C8z8etztNo5E33Qd6n6q3TFe3Z7aPPSer4nOEmADwljdIh
 ; Hash 加盐, 一般在首次启动时自动生成
 HashIDSalt = 2je20slhtLqQXalz7a0mCA9hU0H88B0D7vy3jiW4Zw97z35VBeglaFuDBEwmNma9
+[Redis]
+Server = 127.0.0.1:6379
+Password =
+DB = 0
+[Database]
+Type = $DB_Type
+Host = $DB_Host
+Port = $DB_Port
+User = $DB_User
+Password = $DB_Password
+Name = $DB_Name
+Charset = $DB_Charset
+TablePrefix = $DB_TablePrefix
 EOF
-/root/cloudreve/cloudreve -c /root/cloudreve/conf.ini
+(redis-server &) && (/root/cloudreve/cloudreve -c /root/cloudreve/conf.ini)
